552aeb8466d964eed76fa15aa05dcf33daa65ef6
Надя <nk20001000@gmail.com>
2022-06-29 19:49:47 +0300

    Fix review comments

--- Lesson2/leson2.2.ts
+++ Lesson2/leson2.2.ts
@@ -1,8 +1,11 @@
 type Data = [string, string, string, string][];
-interface IFilters {
+
+interface IFilter {
   square?: string;
   street?: string;
 };
+
+type Filters = IFilter[];
 
 const data: Data = [
   ["36", "не приватизирован", "4 собственника", "Парковая"],
@@ -12,22 +15,29 @@ const data: Data = [
   ["25", "не приватизирован", "2 собственника", "Ленина"]
 ];
 
-const filter = (data: Data, filters: IFilters) => {
-  const { square, street } = filters;
-  
+const filters: Filters = [
+  { square: '40', street: 'Ленина' },
+  { square: '56' },
+];
+
+const filter = (data: Data, filters: Filters) => {
   return data.map((item) => {
-    let result = true;
+    return filters.some((filter) => {
+      const { square, street } = filter;
 
-    if (square && item[0] != square) {
-      result = false;
-    }
+      let result = true;
 
-    if (street && item[3] != street) {
-      result = false;
-    }
-    return result;
+      if (square && item[0] != square) {
+        result = false;
+      }
+
+      if (street && item[3] != street) {
+        result = false;
+      }
+
+      return result;
+    });
   });
 }
 
-console.log(filter( data, { square: '40', street: 'Ленина' } ));
-console.log(filter( data, { square: '56' } ));
+console.log(filter( data, filters));
